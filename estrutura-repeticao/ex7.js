71b12eff87c7accad20c0b80702c3afecc224f51
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2024-05-08 14:46:22 -0300

    Feat: Estrutura de repetição - Exercício 7 feito

---
+++ estrutura-repeticao/ex7.js
@@ -0,0 +1,10 @@
+//7. Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let i = 1, soma = 0;
+
+while(i <= 100) {
+    soma += i;
+    i++;
+}
+
+console.log("Soma dos números de 1 a 100 = " + soma);
